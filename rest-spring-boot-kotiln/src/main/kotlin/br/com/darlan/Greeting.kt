5620a8091720d3b323f030e1a6189165579e6100
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-04-03 21:24:37 -0300

    48-EntendendoAsAnnotations@RestController@Req

--- rest-spring-boot-kotiln/src/main/kotlin/br/com/darlan/Greeting.kt
+++
@@ -1,4 +0,0 @@
-package br.com.darlan
-
-class Greeting(val id: Long, val content: String) {
-}
---
+++ rest-spring-boot-kotiln/src/main/kotlin/br/com/darlan/MathController.kt
@@ -0,0 +1,20 @@
+package br.com.darlan
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/math")
+class MathController {
+
+
+    @GetMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+    fun sum(
+        @PathVariable(value = "numberOne") numberOne: String?,
+        @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double {
+        return 1.0
+    }
+}
